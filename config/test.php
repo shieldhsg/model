b114abcb22413419f330ce14bd91953a28993324
shengguo.hu <782205103@qq.com>
2018-02-05 17:22:23 +0800

    分离框架内容

--- config/test.php
+++ config/test.php
@@ -7,7 +7,8 @@ $db = require __DIR__ . '/test_db.php';
  */
 return [
     'id' => 'basic-tests',
-    'basePath' => dirname(__DIR__),  
+    'basePath' => dirname(__DIR__),
+    'vendorPath'=>__DIR__ . '/../../hsg.vendors/vendor/',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
