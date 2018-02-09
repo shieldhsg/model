4d7fa1e4d5cbdcb7abc9e55c99169acffc3cea84
shengguo.hu <782205103@qq.com>
2018-02-09 16:07:25 +0800

    no message

--- config/test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = require __DIR__ . '/db.php';
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=127.0.0.1;dbname=example';
-
-return $db;
