0ca34bc39aa22b8ec91d80453d76bb498eee5546
shengguo.hu <782205103@qq.com>
2018-02-26 18:18:24 +0800

    fix: 修改bug
    修改bug

--- controllers/ArticlesController.php
+++ controllers/ArticlesController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use app\models\Articles;
-use app\models\search\articlesSearch;
+use app\models\search\ArticlesSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
